d1f232849b9db2c8900422ceccafd34d8fc3e1f4
Shark Bites <shark4bites@gmail.com>
2019-06-12 19:43:40 +1000

    last before merge

--- ARProjectFinal_6893/Assets/Scripts/PuzzleProblem.cs
+++ ARProjectFinal_6893/Assets/Scripts/PuzzleProblem.cs
@@ -1,73 +1,96 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PuzzleProblem : MonoBehaviour
 {
-    float firstNumber;
-    float secondNumber;
-    float thirdNumber = 0;
-    float symbol;
+    public bool isSelectingPuzzle = true; //Condition when player is currently selecting problem
+    public bool isAnswering = false; //Condition when player is currently trying to answer
 
-    void Awake()
+    //List of puzzle pieces
+    //Drag Canvas "PuzzlePieces", child of this to this
+    public GameObject puzzleSelect;
+
+    //List of UIs for answering
+    //Drag Canvas "PlayerUI", child of this to this
+    public GameObject problemAnswer;
+
+    //List of text UIs for displaying problems and answers
+    //Drag UI Text "QText", child of "HeaderBG", child of "PlayerUI" to this
+    public Text problemArea;
+    //Drag UI Text "AText", child of "HeaderBG", child of "PlayerUI" to this
+    public Text answerArea;
+
+    public int firstNumber;
+    public int secondNumber;
+    public int thirdNumber;
+    public int yourAnswer;
+
+    public string targetPuzzleName;
+
+    public void SetProblem(string name)
     {
+        //Setting the problem
+        firstNumber = Random.Range(1, 4);
+        secondNumber = Random.Range(5, 9) - firstNumber;
+        thirdNumber = firstNumber + secondNumber;
+
+        //Writing the problem
+        problemArea.text = firstNumber + " + " + secondNumber;
+
+        //Enable answer mode
+        if (!isAnswering) isAnswering = true;
+        if (isAnswering && !problemAnswer.activeSelf) problemAnswer.SetActive(true);
+
+        //Disable select mode
+        if (isSelectingPuzzle) isSelectingPuzzle = true;
+
+        //Setting the target puzzle
+        if (name != null)
+            if (targetPuzzleName != name)
+                targetPuzzleName = name;
     }
 
-    void Start()
+    public void AnswerAttempt(int number)
     {
+        //Set your answer to the attempt
+        yourAnswer = number;
+        //Set the text to your answer
+        answerArea.text = yourAnswer.ToString();
     }
 
-    public void GiveProblem()
+    public void SubmitAnswer()
     {
-        thirdNumberSetter();
-    }
-
-    void thirdNumberSetter()
-    {
-        while(thirdNumber <= 0)
+        //If correct answer do something
+        if(yourAnswer == thirdNumber)
         {
-            firstNumber = Mathf.Round(Random.Range(1, 9));
-            secondNumber = Mathf.Round(Random.Range(1, 9));
-            symbol = Mathf.Round(Random.Range(1, 4));
-
-            if (symbol == 1) // +, addition
-                thirdNumber = firstNumber + secondNumber;
-
-            else if (symbol == 2 && firstNumber > secondNumber) // -, subtraction
-                thirdNumber = firstNumber - secondNumber;
-
-            else if (symbol == 3) // x, multiply
-                thirdNumber = firstNumber * secondNumber;
-
-            else if (symbol == 4 && firstNumber % secondNumber == 0) // /, divide
-                thirdNumber = firstNumber / secondNumber;
-
-            else
-            { }
+            //Find the puzzle piece and disable the button function of that
+            //If error happens (like no receiver or null reference exception), check the Tag of the puzzle pieces
+            if (GameObject.FindWithTag(targetPuzzleName) != null)
+            {
+                if (GameObject.FindWithTag(targetPuzzleName).GetComponent<Image>() != null)
+                {
+                    GameObject.FindWithTag(targetPuzzleName).GetComponent<Image>().color = new Color32(255, 255, 255, 255);
+                    if (GameObject.FindWithTag(targetPuzzleName).GetComponent<Button>() != null)
+                        if (GameObject.FindWithTag(targetPuzzleName).GetComponent<Button>().enabled)
+                            GameObject.FindWithTag(targetPuzzleName).GetComponent<Button>().enabled = false;
+                }
+            }
+            ResetQA();
         }
-        showProblem();
     }
 
-    void showProblem()
+    public void ResetQA()
     {
-        if (symbol == 1)
-            print(firstNumber + " + " + secondNumber + " =  ?");
-        else if (symbol == 2)
-            print(firstNumber + " - " + secondNumber + " =  ?");
-        else if (symbol == 3)
-            print(firstNumber + " x " + secondNumber + " =  ?");
-        else if (symbol == 4)
-            print(firstNumber + " ÷ " + secondNumber + " =  ?");
-        else
-        print("Solution is " + thirdNumber);
-
-        //Send the problem text to UI Text
-        //SetActive/ActiveSelf InputField of the Canvas + Enter Button
-            //Disable problem button clicking
-            //Disable raycast
-
-        //Enter Button = SendMessage to problem block
-        //Double check answer
-        //If it's correct, send the problem block back
+        firstNumber = 0;
+        secondNumber = 0;
+        thirdNumber = 0;
+        problemArea.text = "";
+        answerArea.text = "";
+        targetPuzzleName = "";
+        isSelectingPuzzle = true;
+        isAnswering = false;
+        problemAnswer.SetActive(false);
     }
 }
